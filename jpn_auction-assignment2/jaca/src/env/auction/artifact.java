7aa2eb276b0e65170a31488ad0047c030ee8020e
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-10-27 03:15:03 -0300

    Add java artifact to repo
    * File was not uploaded due to mistake in gitignore

---
+++ jpn_auction-assignment2/jaca/src/env/auction/artifact.java
@@ -0,0 +1,45 @@
+package auction;
+
+import cartago.Artifact;
+import cartago.OPERATION;
+import cartago.ObsProperty;
+import java.util.ArrayList;
+
+
+public class artifact extends Artifact {
+	
+	private ArrayList<String> bidders = new ArrayList<String>();
+
+    void init() {
+    	this.defineObsProperty("numBidders", bidders.size());
+    	this.defineObsProperty("winner", "");
+    }
+    
+    @OPERATION void startAuction(int value) {
+    	this.defineObsProperty("price",value);
+    }
+
+    @OPERATION void inc(int value){
+        ObsProperty prop = getObsProperty("price");
+        prop.updateValue(prop.intValue()+value);
+    }
+
+    @OPERATION void join(String name) {
+    	bidders.add(name);
+        ObsProperty prop = getObsProperty("numBidders");
+        prop.updateValue(bidders.size());
+    }
+
+    @OPERATION void exit(String name) {
+    	if(bidders.size() > 1)
+            bidders.remove(name);
+    	ObsProperty prop1 = getObsProperty("numBidders");
+        prop1.updateValue(bidders.size());
+
+        if(bidders.size() == 1) {
+        	ObsProperty prop2 = getObsProperty("winner");
+            prop2.updateValue(bidders.get(0));
+        }
+    }
+
+}
